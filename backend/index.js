6b9fa190294fc3a25198b71e1ed9438b44817330
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-03-15 18:59:00 +0530

    + cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, MONGO_URI } from "./config.js";
-import { Book } from "./models/bookModel.js";
 import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
 
 const app = express();
 
@@ -12,13 +12,13 @@ app.use(express.json());
 // Middleware for handling CORS
 
 //Option 1 - Allowing all origins with Default of cors(*)
-// app.use(cors());
+app.use(cors());
 
 //Option 2 - Allowing custom origin
-app.use(cors({ origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"],
-}));
+// app.use(cors({ origin: "http://localhost:3000",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type"],
+// }));
 
 app.get("/", (req, res) => {
   console.log(req);
